3348c5303784e697d6135788b7e6ad9fcb697402
Yasin Erkan <yasinerkan@gmail.com>
2025-06-08 11:46:43 +0200

    fix: use most stable Next.js version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: true
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
